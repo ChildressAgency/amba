b261003de8224e325799b7434491cb43c9480114
childressdev <jcampbell@childressagency.com>
2019-02-08 14:15:18 -0500

    added

---
+++ footer-mildocs.php
@@ -0,0 +1,12 @@
+      </div><?php //.content-side ?>
+    </div>
+  </div>
+  <div id="mildocs-footer">
+    <div class="container">
+      <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" class="mildocs-logo" alt="AMBA Logo" />
+      <p>MILDOCS resources brought to you by <a href="<?php echo esc_url(home_url()); ?>">Association of Military Banks of America</a></p>
+    </div>
+  </div>
+  <?php wp_footer(); ?>
+</body>
+</html>
--- header-mildocs.php
+++ header-mildocs.php
@@ -1,15 +1,38 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <meta http-equiv="cache-control" content="public">
-    <meta http-equiv="cache-control" content="private">
+  <meta http-equiv="cache-control" content="public">
+  <meta http-equiv="cache-control" content="private">
 
-    <?php wp_head(); ?>
+  <?php wp_head(); ?>
 
-    <title><?php echo esc_html(bloginfo('name')); ?></title>
-  </head>
-  <body <?php body_class();?>>
+  <title><?php echo esc_html(bloginfo('name')); ?></title>
+</head>
+<body <?php body_class();?>>
+  <div class="banner"></div>
+  <div id="mildocs" class="container-fluid container-sm-height">
+    <div class="row row-sm-height">
+      <div class="col-sm-4 col-lg-3 col-sm-height nav-side">
+        <?php
+          $mildocs_nav_header = '<h2><a href="' . esc_url(home_url('mildocs')) . '">MILDOCS</a></h2>';
+          $mildocs_nav_args = array(
+            'theme_location' => 'mildocs-nav',
+            'menu' => '',
+            'container' => 'nav',
+            'container_id' => '',
+            'container_class' => 'mildocs-nav',
+            'menu_id' => '',
+            'menu_class' => 'list-unstyled',
+            'echo' => true,
+            'fallback_cb' => false,
+            'items-wrap' => $mildocs_nav_header . '<ul id="%1$s" class="%2$s">%3$s</ul>',
+            'depth' => 1,
+          );
+          wp_nav_menu($mildocs_nav_args);
+        ?>
+      </div>
+      <div class="col-sm-8 col-lg-9 col-sm-height content-side">
