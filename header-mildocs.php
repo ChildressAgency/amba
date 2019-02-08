8075653e53885f05fc768f212cac069a69abada3
childressdev <jcampbell@childressagency.com>
2019-02-08 13:45:58 -0500

    added mildocs demos

---
+++ header-mildocs.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <meta http-equiv="cache-control" content="public">
+    <meta http-equiv="cache-control" content="private">
+
+    <?php wp_head(); ?>
+
+    <title><?php echo esc_html(bloginfo('name')); ?></title>
+  </head>
+  <body <?php body_class();?>>
